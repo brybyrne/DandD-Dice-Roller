1ab9a28549d28514f742f9a43d36ec12be3e252a
brybyrne <brybyrne@cisco.com>
2016-08-10 14:48:12 -0500

    Run flake8 and clean

--- DandD-Dice-Roller.py
+++ DandD-Dice-Roller.py
@@ -1,10 +1,12 @@
 #! /usr/local/bin/python3
 
-#DandD Dice Roller
+import random
+
+# DandD Dice Roller
 #
-#Rather than brand yourself a nerd use this python based
-#D&D die roller. Now you can jump in a game any wher without
-#the sitgma of a pound 'o dice.
+# Rather than brand yourself a nerd use this python based
+# D&D die roller. Now you can jump in a game any wher without
+# the sitgma of a pound 'o dice.
 
 print ("So you forgot your favorite bag of dice.")
 print ("Just enter the number of sides on the required die")
@@ -16,18 +18,15 @@ print ("D12, D10, D8, D6 are used for damage rolls")
 print ("D4 is used for...Well no one really uses D4")
 print ("\n")
 
-import random
-
 dice = int(input('What sided dice are you rolling?:'))
 rolls = int(input('How many dice are you rolling?:'))
 print ("\n")
 print ("Rolling a D%s" % (dice), rolls, "times\n")
 
-while rolls !=0:
+while rolls != 0:
     die1 = random.randint(1, dice)
     print ("You rolled a D%s" % (dice), "and got a", die1)
     rolls -= 1
 
 print ("\n")
 input("Press the enter key to exit.")
-
